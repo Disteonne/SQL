51ceef92c3ef98264f86f2c7bb6d27fcbdfb2a74
Disteonne <diana.razmikovna@yandex.ru>
2020-09-20 23:39:02 +0300

    sql.1

--- src/main/resources/exOne.sql
+++ src/main/resources/exOne.sql
@@ -4,3 +4,19 @@ name VARCHAR(20),
 last_name VARCHAR(20);
 );
 
+________________________________
+ПЕРЕКРЕСТНОЕ СОЕДИНЕНИЕ
+SELECT t.name_toy , b.name_boy  ИМЯ_ТАБЛ.ИМЯ_СТОЛБЦА
+FROM toy AS t
+CROSS JOIN
+boys AS b;
+________________________________
+ВНУТРЕННЕЕ СОЕДИНЕНИЕ
+SELECT boys.boy,toys.toy
+FROM boys
+INNER JOIN
+toys
+ON (WHERE) boys.toy_id=toys.toy_id;
+________________________________
+
+
