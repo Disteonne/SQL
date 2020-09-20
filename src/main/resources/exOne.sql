826f21c03bf6e0872656e1a0288e70a5cd7965ea
Disteonne <diana.razmikovna@yandex.ru>
2020-09-21 00:12:25 +0300

    sql2

--- src/main/resources/exOne.sql
+++ src/main/resources/exOne.sql
@@ -12,11 +12,37 @@ CROSS JOIN
 boys AS b;
 ________________________________
 ВНУТРЕННЕЕ СОЕДИНЕНИЕ
+ЭКВИВАЛЕНТНОЕ
+
 SELECT boys.boy,toys.toy
 FROM boys
 INNER JOIN
 toys
 ON (WHERE) boys.toy_id=toys.toy_id;
+____________     _______________
+SELECT my_cont.email,prof.prof
+FROM my_cont
+INNER JOIN
+prof
+WHERE (ON) my_cont.prof_id=prof.prof_id;
+____________     _______________
+SELECT my_cont.first_name,my_cont.last_name,status.status
+FROM my_cont
+INNER JOIN
+status
+ON my_cont.status_id=status.status_id;
+____________     _______________
+НЕЭКВИВАЛЕНТНОЕ СОЕДИНЕНИЕ
+
+SELECT boys.boy,toys.toy
+FROM boys
+INNER JOIN
+toys
+ON boys.toy_id<> toys.toy_id;
+____________     _______________
+ЕСТЕСТВЕННОЕ СОЕДИНЕНИЕ
+SELECT boys.boy,toys.toy
+FROM boys
+NATURAL JOIN
+toys;
 ________________________________
-
-
