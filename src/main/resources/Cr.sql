009cf0803217c6ff0c9b0c7e2198db75e7e03ec0
Disteonne <diana.razmikovna@yandex.ru>
2020-11-10 22:25:57 +0300

    7vers

--- src/main/resources/Cr.sql
+++ src/main/resources/Cr.sql
@@ -34,3 +34,25 @@ ON d.department_id=e.department_id
 GROUP BY d.department_name
 HAVING AVG(e.salary)>5000
 __________________________________________
+SELECT l.location_id, l.postal_code, c.region_id,COUNT(c.country_id) over(partition by c.region_id) from locations l ,countries c
+where l.country_id=c.country_id AND length(l.postal_code)=length(l.location_id)
+
+SELECT l.location_id, l.postal_code, c.region_id,COUNT(l.location_id) over(partition by  length(l.postal_code)) AS coun from locations l ,countries c
+where l.country_id=c.country_id
+
+SELECT LOC,PC,REG,COUN FROM(
+SELECT l.location_id as loc, l.postal_code as pc, c.region_id as reg,COUNT(l.location_id) over(partition by  c.region_id) AS coun from locations l ,countries c
+where l.country_id=c.country_id
+)
+WHERE length(PC)=COUN
+
+
+
+SELECT loc,pc,reg,COUNT(loc) OVER(partition by reg) from(
+SELECT l.location_id as loc, l.postal_code as pc,length(l.postal_code) as kek, c.region_id as reg from locations l ,countries c
+where l.country_id=c.country_id )
+
+
+SELECT loc,pc,reg,COUNT(loc) over(partition by kek) from(
+SELECT l.location_id as loc, l.postal_code as pc,length(l.postal_code) as kek, c.region_id as reg from locations l ,countries c
+where l.country_id=c.country_id )
